07cab7ce78cd09f168988ec4547ef7167160e643
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-11-08 14:43:22 -0800

    exercise 5

--- exercise_4/exercise-4.sql
+++ exercise_4/exercise-4.sql
@@ -5,7 +5,7 @@ select
   address.address,
   city.city
 from
--- Similar to exercise 3 but with customer, address, city
   customer
+-- Similar to exercise 3 but with customer, address, city
   left join address on address.address_id = customer.address_id
   left join city on city.city_id = address.city_id
--- exercise_5/exercise-5.sql
+++ exercise_5/exercise-5.sql
@@ -1,2 +1,10 @@
 -- find all film titles that an actor with the last_name Davis appears in
+select
+  film.title
+from
+  film
 -- Similar to exercise 3 but with actor, film_actor, film
+  left join film_actor on film_actor.film_id = film.film_id
+  left join actor on actor.actor_id = film_actor.actor_id
+where
+  actor.last_name =  'Davis'
