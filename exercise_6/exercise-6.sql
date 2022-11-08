1961b2edf30c214e232565163cf7b6690a43e5a7
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-11-08 14:54:29 -0800

    exercise 6

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,11 @@
 -- find all film titles rented out by customers with the first_name Roberta
+select
+  film.title
+from
+  film
 -- Similar to exercise 5
+  left join inventory on inventory.film_id = film.film_id
+  left join rental on rental.inventory_id = inventory.inventory_id
+  left join customer on customer.customer_id = rental.customer_id
+where
+  customer.first_name = 'Roberta'
