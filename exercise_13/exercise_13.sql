9c6194a322af2d966d405a985211a97393fed247
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-14 13:40:54 -0800

    finish sql joins tests

--- exercise_13/exercise_13.sql
+++ exercise_13/exercise_13.sql
@@ -1,3 +1,22 @@
 -- the average rental amount by country
 -- include the country name and avg amount
 -- use AVG to average the amount
+select
+  country.country,
+  avg(payment.amount)
+from
+    film
+    left join inventory on inventory.film_id = film.film_id
+    left join rental on inventory.inventory_id = rental.inventory_id
+    left join payment on rental.rental_id = payment.rental_id
+    left join customer on payment.customer_id = customer.customer_id
+    left join address on customer.address_id = address.address_id
+    inner join city on address.city_id = city.city_id
+    inner join country on city.country_id = country.country_id
+group by
+    country.country
+order by
+	avg desc
+limit 
+	10
+;
--- exercise_16/exercise_16.sql
+++ exercise_16/exercise_16.sql
@@ -1,3 +1,18 @@
 -- return the title, description, release year and list of actors returned as a JSON blob
 -- for the movie Airplane Sierra
-    
+select
+  film.title,
+  film.description,
+  film.release_year,
+  json_agg(json_build_object('actor_id', actor.actor_id, 'first_name', actor.first_name, 'last_name', actor.last_name, 'last_update', actor.last_update)) as actors
+from
+    film
+    left join film_actor on film_actor.film_id = film.film_id
+    left join actor on actor.actor_id = film_actor.actor_id
+    where
+    film.title = 'Airplane Sierra'
+group by    
+    film.title,
+    film.description,
+    film.release_year
+;
